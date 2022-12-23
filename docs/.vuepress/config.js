61913fdb38b5e61aada5c35f4194876246d58c20
jgckM <z3515235474@163.com>
2022-12-23 17:46:54 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,39 @@
-module.exports = {
-  title: 'Java学习',
-  description: 'Java Study,坚持努力',
-  base: '/java/'
-}
+import { defineUserConfig, defaultTheme } from 'vuepress'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+
+export default defineUserConfig({
+    title: 'Java',
+    description: 'Java Study,坚持努力',
+    base: '/java/',
+    markdown: {
+        lineNumbers: true,
+    },
+    theme: defaultTheme({
+        logo: '/logo/java.png',
+        nav: [
+            { text: 'Github', link: 'https://github.com/jgckM/java' },
+        ],
+        sidebar: {
+            '/': [
+                'test',
+                'test2'
+            ]
+        },
+        locales: {
+            // 键名是该语言所属的子路径
+            // 作为特例，默认语言可以使用 '/' 作为其路径。
+            '/': {
+                lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+            },
+            home: '/',
+        },
+    }),
+    head: [
+        ['link', { rel: 'icon', href: '/logo/java.png' }],
+    ],
+    plugins: [
+        docsearchPlugin({
+            // 配置项
+        }),
+    ],
+})
